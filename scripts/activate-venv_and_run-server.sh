23f4f639f0f040a68e7411d21688cb9f15b01034
sl5net <sl5softwarelab@gmail.com>
2025-06-29 21:33:52 +0200

    update activate-venv_and_run-server.sh

--- scripts/activate-venv_and_run-server.sh
+++ scripts/activate-venv_and_run-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# activate-venv_and_run-server.sh
 # Exit immediately if a command fails
 set -e
 
@@ -11,6 +11,22 @@ PROJECT_ROOT="$SCRIPT_DIR/.."
 HEARTBEAT_FILE="/tmp/$SCRIPT_firstName.heartbeat"
 SCRIPT_TO_START="$SCRIPT_DIR/../$SCRIPT_firstName.py"
 MAX_STALE_SECONDS=5
+
+
+# Get the currently active window ID BEFORE starting anything.
+ACTIVE_WINDOW_ID=$(xdotool getactivewindow)
+
+# Check if we got a valid ID
+if [ -z "$ACTIVE_WINDOW_ID" ]; then
+    echo "Error: Could not get active window ID."
+    notify-send "Vosk Trigger Error" "Could not identify the active window."
+    exit 1
+fi
+
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
+export DISPLAY=:0
+export XAUTHORITY=${HOME}/.Xauthority
+export DICTATION_SERVICE_STARTED_CORRECTLY="true"
 
 if [ -f "$HEARTBEAT_FILE" ]
 then
@@ -37,7 +53,12 @@ echo "Starting Python server from '$PROJECT_ROOT'..."
 # We run the python script using its absolute path to be safe
 
 echo "Starting service..."
-python3 "$SCRIPT_TO_START" &
+# python3 "$SCRIPT_TO_START" &
+python3 "$SCRIPT_TO_START" --target-window "$ACTIVE_WINDOW_ID" &
+sleep 2
+pkill -9 -f "$PROJECT_ROOT/type_watcher.sh"
+$PROJECT_ROOT/type_watcher.sh &
+
 
 
 
