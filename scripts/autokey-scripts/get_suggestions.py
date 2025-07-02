5eba97ca18838036d6050153706c68a4ea2ff665
sl5net <sl5softwarelab@gmail.com>
2025-07-02 18:19:23 +0200

    update autkey scripts

--- scripts/autokey-scripts/get_suggestions.py
+++ scripts/autokey-scripts/get_suggestions.py
@@ -4,8 +4,8 @@ from pathlib import Path
 import os
 
 readmeBuckup = """
-xdotool key --clearmodifiers ctrl+Left ctrl+shift+Right && sleep 0.1 && xdotool key --clearmodifiers ctrl+c && sleep 0.1 && /home/seeh/projects/py/STT/.venv/bin/python ~/projects/py/STT/get_suggestions.py
-command = 'xdotool click 2 && sleep 0.1 && xdotool key --clearmodifiers "ctrl+c" && sleep 0.1 && /home/seeh/projects/py/STT/.venv/bin/python ~/projects/py/STT/get_suggestions.py'
+xdotool key --clearmodifiers ctrl+Left ctrl+shift+Right && sleep 0.1 && xdotool key --clearmodifiers ctrl+c && sleep 0.1 && /home/seeh/projects/py/STT/vosk-tts/bin/python ~/projects/py/STT/get_suggestions.py
+command = 'xdotool click 2 && sleep 0.1 && xdotool key --clearmodifiers "ctrl+c" && sleep 0.1 && /home/seeh/projects/py/STT/vosk-tts/bin/python ~/projects/py/STT/get_suggestions.py'
 system . exec_command (command)
 Mond
 """
@@ -14,12 +14,12 @@ keyboard.send_keys('<ctrl>+<left>')
 keyboard.send_keys('<ctrl>+<shift>+<right>')
 keyboard.send_keys('<ctrl>+c')
 time.sleep(0.2)
+keyboard.send_keys('<right>')
 
 command = '/home/seeh/projects/py/STT/.venv/bin/python ~/projects/py/STT/get_suggestions.py'
 system.exec_command (command)
 
-#  ( düst | tust | des | Nest | lest | Pest | duzt | West | tost | Rest | döst | käst | fest | teste | säst | Fest | desto | Test )
-# Mond 
+# Haus ( aus | hass | hatz | hausse | ass )
 
 
 
