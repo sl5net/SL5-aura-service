40ee62b855e1af5d55b5acdca8c56fa26efa8e1c
sl5net <sl5softwarelab@gmail.com>
2025-07-02 19:18:51 +0200

    use relative path use {scriptDir}

--- scripts/autokey-scripts/get_suggestions.py
+++ scripts/autokey-scripts/get_suggestions.py
@@ -3,9 +3,12 @@ import time
 from pathlib import Path
 import os
 
+SCRIPT_DIR = Path(__file__).resolve().parent
+
+
 readmeBuckup = """
-xdotool key --clearmodifiers ctrl+Left ctrl+shift+Right && sleep 0.1 && xdotool key --clearmodifiers ctrl+c && sleep 0.1 && /home/seeh/projects/py/STT/vosk-tts/bin/python ~/projects/py/STT/get_suggestions.py
-command = 'xdotool click 2 && sleep 0.1 && xdotool key --clearmodifiers "ctrl+c" && sleep 0.1 && /home/seeh/projects/py/STT/vosk-tts/bin/python ~/projects/py/STT/get_suggestions.py'
+xdotool key --clearmodifiers ctrl+Left ctrl+shift+Right && sleep 0.1 && xdotool key --clearmodifiers ctrl+c && sleep 0.1 && {scriptDir}/.venv/bin/python {scriptDir}/vosk-tts/bin/python ~/projects/py/STT/get_suggestions.py
+command = 'xdotool click 2 && sleep 0.1 && xdotool key --clearmodifiers "ctrl+c" && sleep 0.1 && {scriptDir}/vosk-tts/bin/python ~/projects/py/STT/get_suggestions.py'
 system . exec_command (command)
 Mond
 """
@@ -16,7 +19,7 @@ keyboard.send_keys('<ctrl>+c')
 time.sleep(0.2)
 keyboard.send_keys('<right>')
 
-command = '/home/seeh/projects/py/STT/.venv/bin/python ~/projects/py/STT/get_suggestions.py'
+command = SCRIPT_DIR + '/.venv/bin/python ~/projects/py/STT/get_suggestions.py'
 system.exec_command (command)
 
 # Haus ( aus | hass | hatz | hausse | ass )
