8f209afbdbda4903c8814894e26f8d502a7c1063
sl5net <sl5softwarelab@gmail.com>
2025-07-04 01:06:44 +0200

    use Path.home() in get_suggestions.py

--- scripts/autokey-scripts/get_suggestions.py
+++ scripts/autokey-scripts/get_suggestions.py
@@ -19,7 +19,12 @@ keyboard.send_keys('<ctrl>+c')
 time.sleep(0.2)
 keyboard.send_keys('<right>')
 
-command = SCRIPT_DIR + '/.venv/bin/python ~/projects/py/STT/get_suggestions.py'
+home_dir = Path.home()
+
+command = f"{home_dir}/projects/py/STT/.venv/bin/python {home_dir}/projects/py/STT/get_suggestions.py"
+# clipboard.fill_clipboard(command)
+# {SCRIPT_DIR}/.venv/bin/python {SCRIPT_DIR}/get_suggestions.py
+# maus     Has Huns UAS Hulas Huts /home/seeh/.config/autokey/data/stt/autokey-scripts/.venv/bin/python /home/seeh/.config/autokey/data/stt/autokey-scripts/get_suggestions.py
 system.exec_command (command)
 
 # Haus ( aus | hass | hatz | hausse | ass )
