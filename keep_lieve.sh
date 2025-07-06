78b734cf4601126399b393c0992bae9f8392fc85
sl5net <sl5softwarelab@gmail.com>
2025-07-07 00:34:59 +0200

    keep_lieve.sh keeps the service always on.  workaround because it's not so stable

--- keep_lieve.sh
+++ keep_lieve.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
-# keep_lieve.sh
+#
 # test Okay it's good to see me online today and oh, and it's in English okay English is also nice
 #  I can understand a little bit British English You need from the United States that can speak so well But sometimes need to listen to British or am I correct in English Undies from the United States It's And Trump is also from the United States
 
 echo "keep_lieve.sh checks if dictation_service is running and restarts it when not"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+CONFIG_FILE="$SCRIPT_DIR/config/server.conf"
+SERVER_SCRIPT="$SCRIPT_DIR/scripts/activate-venv_and_run-server.sh"
+
+source "$CONFIG_FILE" # loads Variable PORT
 
 while true
 do
     # ps aux | grep dictation_service
 
     if pgrep -f dictation_service > /dev/null; then
-        nothing=0
-        # echo "dictation_service is running"
+        :
     else
         echo "dictation_service is not running, starting..."
-        ./dictation_service
+        echo "Starting TTS Server on Port $PORT..."
+        #./scripts/activate-venv_and_run-server.sh
+        $SERVER_SCRIPT
         sleep 10
     fi
     #time.sleep(0.40)
     sleep 0.41
 done
 
+#
