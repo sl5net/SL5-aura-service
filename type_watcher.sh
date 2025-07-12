f96deac47605dd097f03947dc7e1e64f2d2715e3
sl5net <sl5softwarelab@gmail.com>
2025-07-12 20:51:08 +0200

    update type_watcher.sh

--- type_watcher.sh
+++ type_watcher.sh
@@ -4,11 +4,21 @@
 DIR_TO_WATCH="/tmp"
 LOCKFILE="/tmp/type_watcher.lock"
 
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+LOGFILE="$SCRIPT_DIR/log/type_watcher.log"
+
+log_message() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOGFILE"
+}
+log_message "Hello fom Watcher"
+
 # --- Lockfile-Logic
 if [ -e "$LOCKFILE" ]; then
     pid=$(cat "$LOCKFILE" 2>/dev/null)
     if [ -n "$pid" ] && ps -p "$pid" > /dev/null; then
-        # echo "Watcher already runs (PID: $pid). Exiting."
+        msg="Watcher already runs (PID: $pid). Exiting."
+        echo "$msg"
+        log_message "$msg"
         exit 0
     fi
     rm -f "$LOCKFILE"
@@ -26,6 +36,7 @@ while true; do
         for f in $files_to_process; do
             if [ -f "$f" ]; then
                 TEXT=$(cat "$f")
+                sleep 0.01
                 rm "$f"
                 if [ -n "$TEXT" ]; then
                     xdotool type --clearmodifiers --delay 0 "$TEXT"
