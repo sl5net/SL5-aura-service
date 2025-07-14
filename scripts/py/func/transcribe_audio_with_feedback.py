0ecffde3ab5f11971b118b2a45c281031a7a4dc8
sl5net <sl5softwarelab@gmail.com>
2025-07-14 03:03:02 +0200

    add notify text

--- scripts/py/func/transcribe_audio_with_feedback.py
+++ scripts/py/func/transcribe_audio_with_feedback.py
@@ -20,7 +20,7 @@ def transcribe_audio_with_feedback(logger, recognizer, LT_LANGUAGE):
     if random() > 0.5:
         notify(f"Listening {LT_LANGUAGE} ...", "Speak now. It will stop on silence.", "low", icon="media-record")
     else:
-        notify(f"", "", "low", icon="media-record")
+        notify(f"Speak now.", "It will stop on silence.", "low", icon="media-record")
 
     is_speech_started = False
     current_timeout = PRE_RECORDING_TIMEOUT
