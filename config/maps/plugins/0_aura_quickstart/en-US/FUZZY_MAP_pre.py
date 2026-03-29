223b4cd2ed912924c8c5a46d7c7a7b72167d1162
sl5net <sl5softwarelab@gmail.com>
2026-03-29 12:39:27 +0200

    docs: add one-line 'Oma-Mode' instruction to Quickstart map

--- config/maps/plugins/0_aura_quickstart/en-US/FUZZY_MAP_pre.py
+++ config/maps/plugins/0_aura_quickstart/en-US/FUZZY_MAP_pre.py
@@ -1,7 +1,1 @@
-# config/maps/plugins/sandbox/en-US/FUZZY_MAP_pre.py
-import re # noqa: F401
-
-FUZZY_MAP_pre = [
-    ('f', 'buid farsrm'),
-    ('Hello','everyone'),
-]
+# TIP: Just type a word below this line (e.g., banana) and save.
