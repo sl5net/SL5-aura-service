4f26008f1e8356f93b5172b7ba602be38f53a49d
sl5net <sl5softwarelab@gmail.com>
2026-05-08 16:49:40 +0200

    add commented snippet to 1_collect_unmatched_training module to all FUZZY_MAP_pre.py

--- config/maps/plugins/vsp_rt/de-DE/FUZZY_MAP_pre.py
+++ config/maps/plugins/vsp_rt/de-DE/FUZZY_MAP_pre.py
@@ -2,6 +2,10 @@
 # config/languagetool_server/maps/plugins/vsp_rt/de-DE/FUZZY_MAP_pr.py
 # https://regex101.com/
 import re # noqa: F401
+#from pathlib import Path as p;import os as o # noqa: E702
+#with open(('C:/tmp'if o.name=='nt'else'/tmp')+'/sl5_aura/sl5net_aura_project_root',encoding='utf-8') as f:PROJECT_ROOT=p(f.read().strip()) # noqa: E702
+#(f'{str(__file__)}', r'^(.*)$', 10,{'on_match_exec':[PROJECT_ROOT / 'config' / 'maps' / 'plugins' / '1_collect_unmatched_training' / 'collect_unmatched.py']}) # noqa: E702
+
 
 # This map uses a hybrid approach:
 # 1. Regex entries are checked first. They are powerful and can be case-insensitive.
