bcab881282e59248cf6f1666b7330553669c6b9e
sl5net <sl5softwarelab@gmail.com>
2026-05-10 01:22:00 +0200

    install python at beginning of ubuntu.sh installer

--- setup/ubuntu_setup.sh
+++ setup/ubuntu_setup.sh
@@ -14,6 +14,9 @@ if [ ! -f "requirements.txt" ]; then
     exit 1
 fi
 
+sudo apt-get update -y
+sudo apt-get install -y python3 python3-pip python3-venv
+
 eval $(python3 scripts/py/setup_config.py)
 echo "LANG 1: $SELECTED_LANG | LANG 2: $SECOND_LANG | EXCLUDE_LANGUAGES: $EXCLUDE_LANGUAGES"
 
