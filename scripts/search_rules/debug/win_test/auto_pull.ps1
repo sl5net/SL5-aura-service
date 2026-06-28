37e3b65ea15256dbee4d4b9eaba3b4232fe1b9af
sl5net <sl5softwarelab@gmail.com>
2026-06-28 20:52:19 +0200

    WIP

--- scripts/search_rules/debug/win_test/auto_pull.ps1
+++ scripts/search_rules/debug/win_test/auto_pull.ps1
@@ -1,4 +1,4 @@
-# scripts\search_rules\debug\win_test\pull_and_test.ps1
+# scripts\search_rules\debug\win_test\auto_pull.ps1
 while ($true) {
     git pull
     Write-Host "Waiting 10 seconds..." -ForegroundColor Gray
