27277cabc14927a7afb825aac5bb784a07de1d40
sl5net <sl5softwarelab@gmail.com>
2026-08-09 14:41:11 +0200

    update modified:   scripts/py/func/checks/integrity_rules.py

--- scripts/py/func/checks/integrity_rules.py
+++ scripts/py/func/checks/integrity_rules.py
@@ -52,6 +52,7 @@ FORBIDDEN_PATTERNS = [
 ]
 
 
+
 INTEGRITY_CHECKS = {
 
     'config/maps/plugins/game/0ad/0ad_actions.py': [
@@ -167,22 +168,18 @@ INTEGRITY_CHECKS = {
         r'--bind="ctrl-g:execute-silent(f={1}; rel=\${f#\$PROJECT_ROOT/}; systemd-run --user --collect --quiet xdg-open \"\$REPO_URL/\$rel#L{2}\")"',
     ],
     'scripts/search_rules/run_rule.sh': [
-
-        r'--bind="alt-g:clear-query"',
         r'--bind="ctrl-backspace:backward-kill-word"',
         r'--bind="ctrl-delete:kill-word"',
         r'--bind="ctrl-left:backward-word"',
         r'--bind="ctrl-right:forward-word"',
+        r'--bind="right-click:execute-silent',
         r'--bind="ctrl-up:up+up+up+up+up"',
         r'--bind="ctrl-down:down+down+down+down+down"',
         r'--bind="home:beginning-of-line"',
         r'--bind="end:end-of-line"',
-
-
-        r'--bind="right-click:execute-silent(bash \$SCRIPT_DIR/proot_control.sh up \$PROJECT_ROOT/config/maps)+reload(bash \$SCRIPT_DIR/run_rule.sh --load-scoped)"',
-        r'--bind="double-click:execute-silent(bash \$SCRIPT_DIR/proot_control.sh set \$PROJECT_ROOT/config/maps \"\$(dirname \$(dirname \$(cat \$HOME/.search_rules_last_path)))\")+clear-query+reload(bash \$SCRIPT_DIR/run_rule.sh --load-scoped)"'
-        r'--bind="alt-i:execute-silent(bash \$SCRIPT_DIR/toggle_gitignore.sh)+reload(bash \$SCRIPT_DIR/run_rule.sh --load-full)"'
-        r'--bind="ctrl-g:execute-silent(f={2}; rel=\${f#\$PROJECT_ROOT/}; systemd-run --user --collect --quiet xdg-open \"\$REPO_URL/\$rel#L{3}\")"',
+        r'--bind="double-click:execute-silent',
+        r'--bind="alt-i:execute-silent',
+        r'--bind="ctrl-g:execute-silent',
     ],
     'scripts/search_rules/preview_rule.py': [
         'print(f"{COLOR_RED}⚠ EXAMPLE \'{example}\' not match Regex{COLOR_RESET}")',
