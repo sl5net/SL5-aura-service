21790b04295dd39142839c2511bd6540a2d80589
sl5net <sl5softwarelab@gmail.com>
2026-08-21 09:09:51 +0200

    test(self_tester): re-enable current-weather test case

--- scripts/py/func/checks/self_tester.py
+++ scripts/py/func/checks/self_tester.py
@@ -259,7 +259,7 @@ def _execute_self_test_core(logger, tmp_dir_aura, lt_url, lang_code):
              'Number with unit', lt=True, prio=TestPrio.HIGH),
         case('lieblingszahlen sind fünf und drei', 'Lieblingszahlen sind 5 und 3', 'numbers 5 and 3', lt=True, prio=TestPrio.ALWAYS),
         case('was ist fünf plus drei', r'Das Ergebnis von 5 plus 3 ist 8\.', 'calc in MAP Wannweil', lt=False, prio=TestPrio.ALWAYS),
-        # case('wie ist das wetter', r'Aktuell in \w+ sind es.*', 'weather plugin', lt=False, prio=TestPrio.ALWAYS),
+        case('wie ist das wetter', r'Aktuell in \w+ sind es.*', 'weather plugin', lt=False, prio=TestPrio.ALWAYS),
         case('bitte reservieren sie einen tisch für zwei personen um acht uhr',
              'Bitte reservieren Sie einen Tisch für 2 Personen um 8 Uhr',
              'Polite request with time and number', lt=True, prio=TestPrio.HIGH),
