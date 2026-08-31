ca13bdfc2bde2c8f960610ebb1c2988b43236e02
sl5net <sl5softwarelab@gmail.com>
2026-08-31 07:09:24 +0200

    use HOME/programs/${APP_NAME}

--- web_install.sh
+++ web_install.sh
@@ -5,7 +5,10 @@ set -e
 trap 'echo -e "\n[INFO] Installation aborted by user."; kill 0 2>/dev/null; exit 130' INT TERM
 
 APP_NAME="sl5-aura-service"
-export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+#export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+
+mkdir -p "${HOME}/programs"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/programs/}"
 export INSTALL_DIR="${XDG_DATA_HOME}/${APP_NAME}"
 REPO_BRANCH="${AURA_BRANCH:-master}"
 REPO_TAR_URL="https://github.com/sl5net/SL5-aura-service/archive/refs/heads/${REPO_BRANCH}.tar.gz"
